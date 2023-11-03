7c2519574a88f579550b647b631bad0c04c64a63
KELVIN MURITHI <kelvinmurithi@live.com>
2023-11-04 02:21:28 +0300

    completed

---
+++ src/context/user.js
@@ -0,0 +1,14 @@
+import React,{useState} from 'react'
+// create the context
+const UserContext = React.createContext();
+
+function UserProvider({children}){
+  const [user, setUser] = useState(null);
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+}
+
+export {UserContext, UserProvider}
